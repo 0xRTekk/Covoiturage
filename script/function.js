ac703e2116fa1a09a6c6b44e8c570daf66956565
vcursay@laposte.net <vcursay@laposte.net>
2018-10-13 17:17:52 +0200

    Edited navbar

---
+++ script/function.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function ($) {
+
+  /*Getting url info*/
+  /*the format of the url should be: http://localhost/Covoiturage/pages/home.php?type=video&category=hot*/
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+    }
+  };
+
+
+
+
+});
