bb4218b1590c3d11a59091d12d4740c3e9334999
vcursay@laposte.net <vcursay@laposte.net>
2018-10-13 17:07:40 +0200

    Added navbar

---
+++ include/NavBar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="/Covoiturage/style/style.css">
+
+    <title></title>
+  </head>
+  <body>
+
+    <ul class="NavBar">
+      <li class="NavBarLi"><a class="NavBarA" href="#home">Home</a></li>
+      <li class="NavBarLi"><a class="NavBarA" href="#browse">Browse Journey</a></li>
+      <li class="NavBarLi"><a class="NavBarA" href="#sumbit"> Submit Journey</a></li>
+      <li class="NavBarLi"><a class="NavBarA" href="#about">Sign In</a></li>
+      <li class="NavBarLi"><a class="NavBarA" href="#about">Sign Up</a></li>
+    </ul>
---
+++ pages/Home.php
@@ -0,0 +1,10 @@
+<?php
+$title = 'Home';
+$currentpage = "home.php";
+include ('../include/navbar.php');// Include the navbar
+?>
+
+
+<?php
+include ('../include/footer.php');//permet d'inclure la navbar et le <head> en une ligne
+?>
