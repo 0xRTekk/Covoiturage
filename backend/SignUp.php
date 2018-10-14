dffbac74b7b7c41966bbf78944e472944e671583
Vincentdecursay <vcursay@laposte.net>
2018-10-14 18:38:16 +0200

    Sign up backend

--- backend/SignUp.php
+++ backend/SignUp.php
@@ -6,18 +6,28 @@ include '../Database/ConnectDatabase.php';
 
 
 if (isset($_POST["SignUpButton"])) {
-  $currentpage = $_POST["Currentpage"];
+  $mel = $_POST["SignUpEmail"];
+  $pseudo = $_POST["SignUpUsername"];
+  $pass = $_POST["SignUpPassword"];
+  $nom = $_POST["SignUpLastname"];
+  $prenom = $_POST["SignUpFirstname"];
+  $tel_mobile = $_POST["SignUpPhone"];
+  $sexe = $_POST["SignUpGender"];
+  $fumeur = $_POST["SignUpSmoking"];
+  $voiture = $_POST["SignUpCar"];
+  $moteur = $_POST["SignUpMotor"];
+  $annee_naissance = $_POST["SignUpYearBorn"];
+  $type = $_POST["SignUpSignUpType"];
+  $confort = $_POST["SignUpComfort"];
 
-  $username = $_POST["SignUpUsername"];
-  $password = $_POST["SignUpPassword"];
-  $email = $_POST["SignUpEmail"];
 
 
-  $req = "insert into users values(null,'$username','$password','$email',NOW(),null,null,0)";
+
+  $req = "insert into membres values('$mel','$pseudo','$pass','$email',NOW(),null,null,0)";
 
   $resultat = mysqli_query($con, $req);
-
-  header( "Location: ../pages/$currentpage" );
+  echo $req;
+//  header( "Location: ../pages/home.php" );
 }
 
 ?>
